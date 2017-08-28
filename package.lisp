d7b5b57680c9c71c8e6c6b1211e9a9e1efbc3d56
Christopher Hebert <hebert.christopherj@gmail.com>
2017-08-28 13:30:10 -0700

    update package exports

--- package.lisp
+++ package.lisp
@@ -3,5 +3,59 @@
 ;;;; Copyright (c) 2017 Christopher Hebert <hebert.christopherj@gmail.com>
 
 (defpackage #:ssdl
-  (:use #:cl))
+  (:use #:cl)
+  (:export
+   #:quit
+   #:init
+   #:display
+   #:clear
+   #:draw-rect
+   #:load-bmp
+   #:flip-none
+   #:flip-horizontal
+   #:flip-vertical
+   #:draw-texture
+   #:free-texture
+   #:draw-color
+   #:poll-event
+   #:key-down?
+   #:key-up?
+   #:repeat?
+   #:scancode
+   #:mouse-motion?
+   #:mouse-button-down?
+   #:mouse-button-up?
+   #:lmb?
+   #:mmb?
+   #:rmb?
+   #:mouse-x
+   #:mouse-y
+   #:quit?
+   #:audio-available
+   #:write-audio
+   #:clear-audio
+   #:stop-audio
+   #:play-audio
+   #:*audio-u8*
+   #:*audio-s8*
+   #:*audio-u16*
+   #:*audio-s16*
+   #:*audio-s32*
+   #:*audio-f32*
+   #:open-audio
+   #:sample-size-in-bytes
+   #:close-audio
+   #:ticks
+   #:delay
+   #:scancode-name
+   #:scancode-from-name
+   #:joy-added?
+   #:joy-removed?
+   #:joy-down?
+   #:joy-up?
+   #:joy-axis?
+   #:joy-id
+   #:joy-button
+   #:joy-axis
+   #:joy-axis-value))
 
