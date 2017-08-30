2180d89ebad7458326960a4d34d8ffdb93dbeb85
Christopher Hebert <hebert.christopherj@gmail.com>
2017-08-30 13:16:02 -0700

    Merge branch 'master' of github.com:chebert/ssdl

--- ssdl.c
+++ ssdl.c
@@ -1,6 +1,9 @@
 #include <SDL2/SDL.h>
 #include <assert.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 static SDL_Window* window;
 static SDL_Renderer* renderer;
